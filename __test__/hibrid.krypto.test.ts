6e6ca8ccffd61930cceb0a97026f5123841262ee
fmolliet <fmolliet@hotmail.com>
2022-08-23 22:50:28 -0300

    feat: adicionando template de lib sobre o projeto

---
+++ __test__/hibrid.krypto.test.ts
@@ -0,0 +1,14 @@
+import HibridKrypto from '../src/index';
+
+describe('Testing Hibrid krypto', () => {
+ 
+
+  test('Encrypting with key', async () => {
+   
+
+    expect(true).toBe(true);
+  });
+
+	
+	
+});
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { createCipheriv, createDecipheriv, scryptSync, randomBytes } from 'crypto';
+
+class HibridKrypto {
+ 
+
+}
+
+export default HibridKrypto;
